993ee4d0f3c08fe5b39a2a8790d3f78648af4fe4
Will Kerwin <drgummybears@outlook.com>
2019-06-26 16:35:22 +0100

    basic set up of proxy ut gotta go work so half done

---
+++ src/main/java/com/kerwin/mymodtutorial/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.kerwin.mymodtutorial.reference;
+
+public class Reference {
+
+    public static final String MODID = "mymodtutorial";
+    public static final String NAME = "My Mod Tutorial";
+    public static final String VERSION = "1.0";
+    public static final String CLIENT_PROXY = "com.kerwin.mymodtutorial.proxy.ClientProxy";
+    public static final String SERVER_PROXY = "com.kerwin.mymodtutorial.proxy.ServerProxy";
+}
